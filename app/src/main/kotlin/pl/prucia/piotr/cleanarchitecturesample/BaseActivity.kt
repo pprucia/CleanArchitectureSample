e1284cb5560e3790aa114114a46d068d9ed83af4
Piotr Prucia <piotr@prucia.pl>
2018-04-10 20:46:08 +0200

    Upgrades and issue cleanup

--- app/src/main/kotlin/pl/prucia/piotr/cleanarchitecturesample/BaseActivity.kt
+++ app/src/main/kotlin/pl/prucia/piotr/cleanarchitecturesample/BaseActivity.kt
@@ -1,5 +1,7 @@
 package pl.prucia.piotr.cleanarchitecturesample
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 
+@SuppressLint("Registered")
 open class BaseActivity : AppCompatActivity()
